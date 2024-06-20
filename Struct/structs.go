ec71a065460aa30bfe328c56a6512e59382cca73
Marek Majdak <22301547+marekm1844@users.noreply.github.com>
2024-06-21 00:25:52 +0200

    Added Struct

---
+++ Struct/structs.go
@@ -0,0 +1,40 @@
+package structlab
+
+import "math"
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+
+
+func Perimiter(rec Rectangle) float64 {
+
+	return 2 * (rec.Width + rec.Height)
+}	
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
